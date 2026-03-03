9a628c372e3febd3995ade37742ae26f222e734b
Alex <leksfix@gmail.com>
2026-03-03 19:47:50 +0100

    Add color package

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	"hexlet-go/greeting"
+	"github.com/fatih/color"
 )
 
 func main () {
 	fmt.Println(greeting.Hello())
+	color.Red(greeting.Hello())
+	c := color.New(color.BgGreen, color.FgYellow)
+	c.Println("TEST TEST TEST!")
 }
